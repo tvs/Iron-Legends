be5c428d52b8e469280d358b02bcff0af86aa14c
bhadresh <bhadresh4u@gmail.com>
2009-11-17 15:44:17 -0800

    Added Team Star, changed ATSprite offset to keep star from rotating

--- src/jig/ironLegends/core/ATSprite.java
+++ src/jig/ironLegends/core/ATSprite.java
@@ -14,10 +14,10 @@ import jig.engine.util.Vector2D;
 public class ATSprite extends Sprite {
 	private double m_rotationRad = 0;
 	private Vector2D m_offsetToRotation;
-	private boolean m_bAbsRotation = false;	
-	private Vector2D m_offset = new Vector2D(0,0);
-	private Vector2D m_rotationOffset  = new Vector2D(0,0);
-	
+	private boolean m_bAbsRotation = false;
+	private Vector2D m_offset = new Vector2D(0, 0);
+	private Vector2D m_rotationOffset = new Vector2D(0, 0);
+
 	ATSprite(final List<ImageResource> frameset) {
 		super(frameset);
 		setoffsetToRotation();
@@ -28,18 +28,17 @@ public class ATSprite extends Sprite {
 		setoffsetToRotation();
 	}
 
-	public void setAbsRotation(boolean bAbsolute)
-	{
+	public void setAbsRotation(boolean bAbsolute) {
 		m_bAbsRotation = bAbsolute;
 	}
-	
-	// set the location where the ATSprite's rotation center will be attached to the MultiSpriteBody
-	public void setOffset(Vector2D vOffset)
-	{
+
+	// set the location where the ATSprite's rotation center will be attached to
+	// the MultiSpriteBody
+	public void setOffset(Vector2D vOffset) {
 		m_offset = vOffset;
 	}
-	public void setRotation(double rotRad) 
-	{
+
+	public void setRotation(double rotRad) {
 		m_rotationRad = rotRad;
 	}
 
@@ -47,25 +46,17 @@ public class ATSprite extends Sprite {
 		return m_rotationRad;
 	}
 
-	// computes the "center" of the sprite as the starting point for where to rotate
+	// computes the "center" of the sprite as the starting point for where to
+	// rotate
 	private void setoffsetToRotation() {
 		this.m_offsetToRotation = new Vector2D(width / 2.0, height / 2.0);
 	}
 
 	// offset from sprite's center to the rotation point
-	public void setRotationOffset(Vector2D v)
-	{
+	public void setRotationOffset(Vector2D v) {
 		m_rotationOffset = v;
 	}
-	/*
-	public void setoffsetToRotation(Vector2D m_offsetToRotation) {
-		this.m_offsetToRotation = m_offsetToRotation;
-	}
 
-	public Vector2D getoffsetToRotation() {
-		return m_offsetToRotation;
-	}
-	 */
 	public void render(final RenderingContext rc, double dBaseRotationRad) {
 		double prevRotation = m_rotationRad;
 		if (!m_bAbsRotation)
@@ -77,24 +68,24 @@ public class ATSprite extends Sprite {
 	@Override
 	public void render(final RenderingContext rc) {
 		// adjust at
-		// offset to position
 		// offset to center of rotation
 		// rotate
-		// offset back
-		
-		AffineTransform at = AffineTransform.getTranslateInstance(
-				  position.getX() + m_offset.getX() + m_offsetToRotation.getX()
-				, position.getY() + m_offset.getY() + m_offsetToRotation.getY()
-				);
+		// offset back rotation center
+		// offset to offset
+
+		AffineTransform at = AffineTransform.getTranslateInstance(position
+				.getX()
+				+ m_offsetToRotation.getX(), position.getY()
+				+ m_offsetToRotation.getY());
 		at.rotate(m_rotationRad);
 		at.translate(-m_rotationOffset.getX(), -m_rotationOffset.getY());
 		at.translate(-m_offsetToRotation.getX(), -m_offsetToRotation.getY());
-		
+		at.translate(m_offset.getX(), m_offset.getY());
+
 		frames.get(visibleFrame).render(rc, at);
 	}
 
-	public Vector2D getOffset() 
-	{
+	public Vector2D getOffset() {
 		return m_offset;
 	}
 }
